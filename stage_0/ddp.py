60828234a38549c4b2b162eb8f986d0b3666d0bb
Zeyu Long <antinomy20001@gmail.com>
2025-11-28 23:52:18 +0800

    update ddp.py

--- stage_0/ddp.py
+++ stage_0/ddp.py
@@ -104,7 +104,6 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=5)
     parser.add_argument("--batch-size", type=int, default=128)
-    parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--data-dir", type=str, default="/tmp/data")
     parser.add_argument("--save-path", type=str, default="/tmp/resnet18_mnist_ddp_multi_node.pth")
     parser.add_argument("--seed", type=int, default=42)
@@ -112,6 +111,7 @@ def main():
     args = parser.parse_args()
 
     set_seed(args.seed)
+
     torch.backends.cudnn.benchmark = True
 
     backend, rank, world_size, local_rank, device = setup_dist()
@@ -126,8 +126,9 @@ def main():
 
     model = build_model().to(device)
     ddp_model = DDP(model, device_ids=[local_rank] if device.type == "cuda" else None, output_device=local_rank if device.type == "cuda" else None)
+
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(ddp_model.parameters(), lr=args.lr)
+    optimizer = optim.Adam(ddp_model.parameters(), lr=1e-3)
 
     for epoch in range(1, args.epochs + 1):
         train_sampler.set_epoch(epoch)
@@ -146,3 +147,39 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+'''
+假设这个文件叫ddp.py，相比于单卡直接python3 ddp.py，我们在多卡情况下要使用torchrun来运行
+
+torchrun \
+    --nnodes=1 --nproc_per_node=4 --node_rank=0 --master_addr=0.0.0.0 --master_port=23333 \
+    ddp.py \
+    --epochs 233333 --batch-size 128 --data-dir /tmp/data --save-path /tmp/resnet18_mnist_ddp_multi_node.pth
+
+第二行参数是torchrun的参数，表示了Node数量为1，每个Node上有4个进程，进程的rank为0，主Node的地址为0.0.0.0，主Node的端口为23333
+1. nnodes=1 表示只有1个Node（Node=机器）
+2. nproc_per_node=4 表示每个Node上有4个进程（我们目前1 进程=1 GPU）
+3. node_rank=0 表示运行这条命令的Node的rank为0
+4. master_addr=0.0.0.0 表示主Node的地址为0.0.0.0
+5. master_port=23333 表示主Node的端口为23333
+
+也就是说，有多少个Node就要运行多少次，比如说我有2个机器一共8张卡，每个机器4张，那么我就要在两个机器上都运行torchrun，其中：
+第一个Node：
+torchrun \
+    --nnodes=2 --nproc_per_node=4 --node_rank=0 --master_addr=0.0.0.0 --master_port=23333 \
+    ddp.py \
+    --epochs 233333 --batch-size 128 --data-dir /tmp/data --save-path /tmp/resnet18_mnist_ddp_multi_node.pth
+
+第二个Node：
+torchrun \
+    --nnodes=2 --nproc_per_node=4 --node_rank=1 --master_addr=0.0.0.0 --master_port=23333 \
+    ddp.py \
+    --epochs 233333 --batch-size 128 --data-dir /tmp/data --save-path /tmp/resnet18_mnist_ddp_multi_node.pth
+
+参与训练的所有Node都运行torchrun后才会进入ddp.py，具体原理我们会在后续的章节中介绍
+
+注意这里的master_addr和master_port需要是一个所有Node都能访问的ip和port，所有的Node torch就是用这个master来进行互相发现的
+
+第四行是ddp.py的参数，--epochs 233333表示训练233333轮，--batch-size 128表示每个批次有128个样本，--data-dir /tmp/data表示数据目录为/tmp/data，--save-path /tmp/resnet18_mnist_ddp_multi_node.pth表示保存模型的路径为/tmp/resnet18_mnist_ddp_multi_node.pth
+
+'''
